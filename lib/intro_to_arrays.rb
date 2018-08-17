f5aff0a78a59752a07df883f01199785b56da319
braj011 <bhagyaraj01@gmail.com>
2018-08-17 17:04:09 +0000

    automatically backed up by learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,19 @@
+def instantiate_new_array
+  @my_new_array = []
+  @my_new_array 
+end 
+
+def array_with_two_elements
+  my_two_array = [1,2]
+  my_two_array
+end 
+
+def first_element(array)
+  f = array.first
+  f
+end 
+
+def third_element(array)
+  t = array[2]
+  third_element
+end 
